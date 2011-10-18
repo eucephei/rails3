ceab31e0f9b23bbc2111874d6f5c8b448ef2052b
eucephei <eucephei@gmail.com>
2011-10-18 10:36:51 -0700

    Refactor Delegator

--- rails_projects/store/app/models/product.rb
+++ rails_projects/store/app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   
-  def self.search(query)
-    where("name like ?", "%#{query}%")
+  def self.search(params)
+    # where("name like ?", "%#{params}%")
+    products = scoped
+    products = products.where("name like ?", "%" + params[:name] + "%") if params[:name]
+    products = products.where("price >= ?", params[:price_gt]) if params[:price_gt]
+    products = products.where("price <= ?", params[:price_lt]) if params[:price_lt]
+    products
   end
+  
 end
