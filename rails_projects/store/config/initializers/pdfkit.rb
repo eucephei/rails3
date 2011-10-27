f328891ea37aaaa11a7062a0c8ba495658d80cb1
eucephei <eucephei@gmail.com>
2011-10-26 17:00:01 -0700

    PDFKit config

--- rails_projects/store/config/initializers/pdfkit.rb
+++ rails_projects/store/config/initializers/pdfkit.rb
@@ -1,5 +1,6 @@
 PDFKit.configure do |config| 
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf-0.9.9-OS-X.i368' 
+  # config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf-0.9.9-OS-X.i368' 
+  config.wkhtmltopdf = Rails.root.join('app','bin','wkhtmltopdf-0.9.9-OS-X.i368').to_s
   config.default_options = { 
     :page_size => 'Legal', 
     :print_media_type => true 
