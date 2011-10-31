c73d569b94a088bf1ac0552bf27176bec3764204
eucephei <eucephei@gmail.com>
2011-10-31 14:36:36 -0700

    controller

--- rails_projects/store/app/controllers/products_controller.rb
+++ rails_projects/store/app/controllers/products_controller.rb
@@ -1,56 +1,50 @@
 class ProductsController < ApplicationController
+  
+  respond_to :html, :json, :xml
+  
   # GET /products
   # GET /products.json
   def index
     @products = Product.search(params)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
+    
+    respond_with(@products)
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @product }
-    end
+    
+    respond_with(@products)
   end
 
   # GET /products/new
   # GET /products/new.json
   def new
     @product = Product.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end
+    
+    respond_with(@product)
   end
 
   # GET /products/1/edit
   def edit
     @product = Product.find(params[:id])
+    respond_with(@product) do |format|
+      format.xml { render :text => @product.to_xml }
+    end
   end
 
   # POST /products
   # POST /products.json
   def create
     @product = Product.new(params[:product])
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render json: @product, status: :created, location: @product }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    
+    if @product.save
+      cookies.permanent[:last_product_id] = @product.id
+      flash[:notice] = "Successfully created product."
     end
+    
+    respond_with(@product)
   end
 
   # PUT /products/1
@@ -58,15 +52,11 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
 
-    respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update_attributes(params[:product])
+      flash[:notice] = "Successfully updated product."
     end
+    
+    respond_with(@product, :location => products_url)
   end
 
   # DELETE /products/1
@@ -74,10 +64,8 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-
-    respond_to do |format|
-      format.html { redirect_to products_url }
-      format.json { head :ok }
-    end
+    flash[:notice] = "Successfully destroyed product."
+    
+    respond_with(@product, :responder => MyResponder)
   end
 end
