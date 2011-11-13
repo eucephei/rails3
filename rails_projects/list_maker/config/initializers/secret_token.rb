b03c232edc1cf20bd5ea366e86f3f485dcb3e04b
eucephei <eucephei@gmail.com>
2011-11-13 13:49:09 -0800

    Initial commit

---
+++ rails_projects/list_maker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListMaker::Application.config.secret_token = '4669cc6397d14a6fa8a0992c7d1c589a9bbf1f224a67f9348c43f9fe9e6de74a4fcb6335c7041aef38d9e49de080c3712d796f48e7060f6a1f04240a2303611a'
