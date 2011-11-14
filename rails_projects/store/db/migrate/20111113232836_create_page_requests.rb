0daa751cb51fdc3cd8fdc570418932ec3f596a25
eucephei <eucephei@gmail.com>
2011-11-13 16:01:18 -0800

    ActiveSupport::Notifications

--- rails_projects/store/db/migrate/20111113232836_create_page_requests.rb
+++ rails_projects/store/db/migrate/20111113232836_create_page_requests.rb
@@ -2,9 +2,9 @@ class CreatePageRequests < ActiveRecord::Migration
   def change
     create_table :page_requests do |t|
       t.string :path
-      t.float :page_duration
-      t.float :view_duration
-      t.float :db_duration
+      t.float :page_duration, :precision => 8, :scale => 4
+      t.float :view_duration, :precision => 8, :scale => 4
+      t.float :db_duration, :precision => 8, :scale => 4
 
       t.timestamps
     end
