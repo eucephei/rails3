aef383dbd3ce81f3bd2513ca06baa29ffc79d2de
eucephei <eucephei@gmail.com>
2011-11-16 15:02:41 -0800

    paper_trail

---
+++ rails_projects/shop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = '40265361d8582b133a1bcaa3d0199cade4a6d0daec7199d9c43077fa9249faedd800621e2ea06a7508b41789747b9ba7dbe72b5594b1c47d21e0183c5c0eb183'
