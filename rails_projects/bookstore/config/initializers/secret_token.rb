86fa49a7df2f32196df9d2c49804bf1b96afaa6b
eucephei <eucephei@gmail.com>
2011-11-17 15:58:09 -0800

    tokeninput.js

---
+++ rails_projects/bookstore/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '14daffe9d394dc66d464b1657e1ffcda41a00dec1105801c36ce7d2bb1df715dfdefc8e26bd7950be765030c4581a0c5fbb71509c442e966f29a62b1e87bd422'
