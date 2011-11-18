def41fb7bd3cf84a1245449ac4ff0304c5a4e731
eucephei <eucephei@gmail.com>
2011-11-18 15:37:44 -0800

    initial commit

---
+++ rails_projects/chatter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatter::Application.config.secret_token = '06d8ea97537acf98981d5e462e7fefc5e4f5dac5b7999204e4396317c851967af903d7ee938df583ca7b2ac50d3586c3f948b9c8fcdc13321cd17c20aee1d644'
