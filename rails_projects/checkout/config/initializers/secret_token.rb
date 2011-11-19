ef22492a85326e76c4f9501d324b4e7ceec29d88
eucephei <eucephei@gmail.com>
2011-11-19 13:41:36 -0800

    initial commit

---
+++ rails_projects/checkout/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkout::Application.config.secret_token = '05e0a7034b37bbf11de31c969c62ed6241f6305eb245f44fcd9c232ae35df797f7cc5ac6bf3c52205e52898bd020bef1a9a76602fb1a0d9320f646c4bfd85077'
