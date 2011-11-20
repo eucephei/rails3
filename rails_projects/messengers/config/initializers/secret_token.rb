778e98a845240fbc2aada0dc61fc7e6eb898a939
eucephei <eucephei@gmail.com>
2011-11-20 15:49:06 -0800

    initial commit

---
+++ rails_projects/messengers/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Messengers::Application.config.secret_token = '0201034a1dbf2225b24d417000e64d93b9624fac3372e79e1f1c8fe50c5053c9803feb52ecd1681885df76e46d91b7082aba6318b99bbffd5511ea4ea617bbbd'
