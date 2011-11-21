1a9bb579ec684ce0979ad9a3c56781b7f3b2994c
eucephei <eucephei@gmail.com>
2011-11-21 12:28:02 -0800

    initial commit

---
+++ rails_projects/validator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Validator::Application.config.secret_token = '6649f8a7fce0318aec2a781d00141109cff53371d16b2767f3ca5d4c2b25f133fc94dcaf0e7100b18311b230513841a7a893283ba84326a5cab5040fc036bc2f'
