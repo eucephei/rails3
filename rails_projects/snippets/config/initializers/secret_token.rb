1297d5949337459ec737efa1503480f50af3fb92
eucephei <eucephei@gmail.com>
2011-11-25 14:39:43 -0800

    initial commit

---
+++ rails_projects/snippets/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snippets::Application.config.secret_token = '662aae858489bd51b46e6e6eb1f0c3c444ddbf1a1df73acb8aaf56e4c6186c718f230c5969f1cc81a6fbe2058c6d3f44da3fc14ccf542eecaa138ca28839be2b'
