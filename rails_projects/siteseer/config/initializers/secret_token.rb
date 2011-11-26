ebc7d380bb5294fa9e58062d0bdcd79eda5b67e0
eucephei <eucephei@gmail.com>
2011-11-26 15:38:07 -0800

    initial commit

---
+++ rails_projects/siteseer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteseer::Application.config.secret_token = 'cbf83a76b827d80029459894b21c98addcdfc5003986119e48bf18b8ff70cac180cdd04843239bc3ae702f773e3f422bd332193358b1168d6969abf028ec34cb'
