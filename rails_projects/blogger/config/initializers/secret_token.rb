424e65f0a8138ec407e0d161f668f588961e07ea
eucephei <eucephei@gmail.com>
2011-11-28 14:58:24 -0800

    initial commit

---
+++ rails_projects/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '2504af8ff9ca23d6e16aef65d0dbee822aa5c0482a10e5520298d088eac7ded20cefe35752a9d9b63ff2e4d7d0abf1b641d3e372bd0285560ff4c0922746b447'
