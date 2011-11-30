769868274941ce33659ee575a536e2cdc13712d7
eucephei <eucephei@gmail.com>
2011-11-29 16:03:34 -0800

    initial commit

---
+++ rails_projects/authenticate/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authenticate::Application.config.secret_token = 'a8909b439f5cfaf5cb9d4df9922e6edd51372b24d01df529f57d1bc55af9f55a7f29d5c98ca290579fafd5882e8389658d809f93f0298edf8a3753fb45539eea'
