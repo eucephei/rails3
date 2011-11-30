f584742259deb1b5804d102a20867d7b837bb10d
eucephei <eucephei@gmail.com>
2011-11-30 14:54:33 -0800

    initial commit

---
+++ rails_projects/store_admin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreAdmin::Application.config.secret_token = '0a3c223a559e548adc860539b8a66d309f3bd7ee52ef90cfaac9efb6adb070f2a6d379e5250d1a3e38989b50dcbe02d042a2f0ea15180b750d32b89faad3d400'
