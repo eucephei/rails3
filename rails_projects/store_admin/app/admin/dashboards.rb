4881856476b3156b2ff9017a1f30fc4d17b1bffc
eucephei <eucephei@gmail.com>
2011-11-30 15:27:42 -0800

    active_admin custom

--- rails_projects/store_admin/app/admin/dashboards.rb
+++ rails_projects/store_admin/app/admin/dashboards.rb
@@ -1,5 +1,15 @@
 ActiveAdmin::Dashboards.build do
 
+  section "Recent Products" do
+    table_for Product.order("released_at desc").limit(5) do
+      column :name do |product|
+        link_to product.name, [:admin, product]
+      end
+      column :released_at
+    end
+    strong { link_to "View All Products", admin_products_path }
+  end
+  
   # Define your dashboard sections here. Each block will be
   # rendered on the dashboard in the context of the view. So just
   # return the content which you would like to display.
