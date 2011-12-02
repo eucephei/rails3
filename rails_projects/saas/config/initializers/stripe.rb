16c8fd3b8647a9da4b3ab6ce01d29f76371570bc
eucephei <eucephei@gmail.com>
2011-12-02 15:21:19 -0800

    Billing w/ stripe

---
+++ rails_projects/saas/config/initializers/stripe.rb
@@ -0,0 +1,4 @@
+Stripe.api_key = "tGaNWsIG3Qy6zvXB8wv4rEcizJpSXjF4"      # API Keys { test, secret }
+STRIPE_PUBLIC_KEY = "pk_KcSyS2qPWdT5SdrwkQg0vNSyhZgqP"   # API Keys { test, publishable }
+
+# https://manage.stripe.com/account 
--- rails_projects/saas/db/schema.rb
+++ rails_projects/saas/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111202215754) do
+ActiveRecord::Schema.define(:version => 20111202222109) do
 
   create_table "plans", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20111202215754) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_customer_token"
   end
 
 end
