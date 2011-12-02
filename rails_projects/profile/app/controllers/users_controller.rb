54a046e14aeee956879c53b87bfd3e1c64d12046
eucephei <eucephei@gmail.com>
2011-12-01 17:27:37 -0800

    Draper, Bluecloth, RedCarpet

--- rails_projects/profile/app/controllers/users_controller.rb
+++ rails_projects/profile/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    @user = UserDecorator.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
