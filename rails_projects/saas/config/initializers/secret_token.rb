915f0eed3c5975f841ccb42bf5800ca40540bf06
eucephei <eucephei@gmail.com>
2011-12-02 14:10:59 -0800

    initial commit

---
+++ rails_projects/saas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saas::Application.config.secret_token = '59a8ba2ec16ba0d04cde83a4eab357f30139f10acdda633318cdcd479e804f377eeb98f34d304482022285efafebdcf1388fdf026d7e77fd5c4cb338f08cd0ad'
