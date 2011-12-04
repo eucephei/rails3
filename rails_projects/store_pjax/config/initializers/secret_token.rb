cc1816c6d70b5460a0a86e8c10b14e5530e78670
eucephei <eucephei@gmail.com>
2011-12-04 14:20:06 -0800

    Pjax

---
+++ rails_projects/store_pjax/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StorePjax::Application.config.secret_token = 'b023a6e136adc0f739c58b36598341aa994cc7079e6d3a5ff4af61e366119ac5ea1ad3dd1e38d1cb1e6bc5cffb57fe6c2e1d65d40ebefdbc08b4a6ed683008e3'
