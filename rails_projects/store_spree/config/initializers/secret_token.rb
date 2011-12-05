3cbebc2887259734d4d79ea0a01e45adb6f310a8
eucephei <eucephei@gmail.com>
2011-12-05 13:49:38 -0800

    initial commit

---
+++ rails_projects/store_spree/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreSpree::Application.config.secret_token = 'bdb55baf5c07ac2b50a1d48f1fd71e1ec2b69f476b59eba4d0b8a731185c2032f6df2099fbefd9b0d70b5ddea07172c6c546cbf936343f72b240c18b5a672f00'
