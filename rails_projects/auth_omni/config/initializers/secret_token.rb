da618923b36598a46ca7105e665f683f5756319d
eucephei <eucephei@gmail.com>
2011-12-05 15:44:44 -0800

    initial commit

---
+++ rails_projects/auth_omni/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthOmni::Application.config.secret_token = '8db7d20688858d3bfe2c3b5c06aa3f8badc7964a2df4565e91d57d74ced20af1a7817f5aa29341880f6ac99e93d448333f101e668c0029484a3525f7eed1111c'
