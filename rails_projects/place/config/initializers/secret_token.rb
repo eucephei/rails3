a14e7253d2598af99f02d31a1d5eaaa316c4e6b4
eucephei <eucephei@gmail.com>
2011-12-05 14:56:08 -0800

    initial commit

---
+++ rails_projects/place/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Place::Application.config.secret_token = '60131ce3f37ec97886eb68c6703508d780e1e509393e1f19d644208aeadd6b9bab31e4acf1d3893f09efe577d534e6cdedb800c040a721f322ab81969e6b1798'
