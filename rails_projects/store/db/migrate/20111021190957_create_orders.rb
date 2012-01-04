a0e72a6ab311fa67708ae6ba7a461edcdcabe497
eucephei <eucephei@gmail.com>
2012-01-04 15:14:10 -0800

    Update rails_projects/store/db/migrate/20111021190957_create_orders.rb

--- rails_projects/store/db/migrate/20111021190957_create_orders.rb
+++ rails_projects/store/db/migrate/20111021190957_create_orders.rb
@@ -1,10 +1,11 @@
-class CreateOrders < ActiveRecord::Migration
-  def change
-    create_table :orders do |t|
-      t.string :shipping_name
-      t.string :billing_name
 
-      t.timestamps
-    end
-  end
-end
+#class CreateOrders < ActiveRecord::Migration
+#  def change
+#    create_table :orders do |t|
+#      t.string :shipping_name
+#      t.string :billing_name
+
+#      t.timestamps
+#    end
+#  end
+# end
